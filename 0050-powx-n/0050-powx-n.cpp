2ab90cf7cc2e4f7f29c02b7b487039e4224e133e
Nandini675 <144613076+Nandini675@users.noreply.github.com>
2025-05-21 05:37:31 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.47%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
-    double power(double x, long n)
-    {
-        if (n == 0)
-            return 1;
-        if (n == 1)
-            return x;
-        if (n % 2 == 0)
-            return power(x * x, n / 2);
-        else
-            return x * power(x * x, (n - 1) / 2);
+    double myPow(double x, int n) {
+       if(n==0) return 1;
+       long N= n;
+       if( N<0){
+        N=-N;
+        x= 1/x;
+       }
+       if(N%2==0) return myPow(x*x,N/2);
+       else return x*myPow(x,N-1);
+
     }
-    double myPow(double x, int n)
-    {
-        if (n < 0)
-        {
-            x = 1 / x;
-            long n1 = (long)n;
-            n1 = -n1;
-            return power(x , n1);
-        }
-        else
-        {
-            return power(x, long(n));
-        }
-    }
+
+  
 };
