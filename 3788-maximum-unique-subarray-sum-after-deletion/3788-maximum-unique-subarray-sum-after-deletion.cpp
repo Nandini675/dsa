cd385a8edbcee3d1d739c9d750865e0609402c99
Nandini675 <144613076+Nandini675@users.noreply.github.com>
2025-07-25 23:41:34 +0530

    Time: 6 ms (5.91%), Space: 29.7 MB (23.82%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int maxSum(vector<int>& nums) {
-        int n= nums.size();
-        int maxsum=0, maxi=INT_MIN;
-         for(int i =0;i<n;i++){
-             maxi= max(maxi,nums[i]);}
-            if( maxi<0) return maxi;
-        for(int i =0;i<n;i++){
-        
- set<int>st;int sum=0;
-for(int j=i;j<n;j++){
-     if(nums[j]<0) continue;
-     if(st.find(nums[j])!=st.end()) continue;
-     st.insert(nums[j]);
-     sum+= nums[j];
-    
+        int n = nums.size();
+        unordered_set<int> st;
+       
+ for(int i=0;i<n;i++){
+    st.insert(nums[i]);
+
+ }
+ int mini= INT_MIN;
+ int sum=0;
+for(auto i: st){
+     if(i>0) sum+=i;
+     else  mini= max(mini,i);
 }
- maxsum= max(maxsum, sum);
-        }
-        return maxsum;
+       
+
+        return  sum==0? mini: sum;
     }
 };
