d70197ca21ab75d47cfa31355397217f8f64e25a
Nandini675 <144613076+Nandini675@users.noreply.github.com>
2025-08-16 23:47:45 +0530

    Time: 0 ms (100%), Space: 8.8 MB (97.05%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+          string res="";
+           int cnt=0;
+            for( auto c: s){
+                 if( c=='('){
+                     if(cnt>0) res+=c;
+                     cnt++;
+                 }
+                 else{
+                     cnt--;
+                     if(cnt>0) res+=c;
+                 }
+            }
+            return res;
+    }
+};
